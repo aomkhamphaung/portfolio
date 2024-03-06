1a4d9482267612a3fe40d45c6d23c07c1974191d
aomkhamphaung <akhamphaung@gmail.com>
2024-03-06 09:02:22 +0630

    update: social links

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,38 +1,64 @@
-import React from 'react'
-import './footer.css'
+import React from "react";
+import "./footer.css";
 
 const Footer = () => {
-    return (
-        <footer className="footer">
-            <div className="footer__container container">
-                <h1 className="footer__title">Aom Kham Phaung</h1>
+  return (
+    <footer className="footer">
+      <div className="footer__container container">
+        <h1 className="footer__title">Aom Kham Phaung</h1>
 
-                <ul className="footer__list">
-                    <li>
-                        <a href="#about" className='footer__link'>About</a>
-                    </li>
+        <ul className="footer__list">
+          <li>
+            <a href="#about" className="footer__link">
+              About
+            </a>
+          </li>
 
-                    <li>
-                        <a href="#portfolio" className='footer__link'>Projects</a>
-                    </li>
+          <li>
+            <a href="#portfolio" className="footer__link">
+              Projects
+            </a>
+          </li>
 
-                    <li>
-                        <a href="#contact" className='footer__link'>Contact</a>
-                    </li>
-                </ul>
+          <li>
+            <a href="#contact" className="footer__link">
+              Contact
+            </a>
+          </li>
+        </ul>
 
-                <div className="footer__social">
-                    <a href="https://www.instagram.com/aom_kham_phaung" target='blank' className='footer__social-link'><i class="uil uil-instagram"></i></a>
+        <div className="footer__social">
+          <a
+            href="mailto:aomkhamphaung.dev@gmail.com"
+            target="blank"
+            className="footer__social-link"
+          >
+            <i class="uil uil-envelope"></i>
+          </a>
 
-                    <a href="https://www.facebook.com/aomkhamphaung" target='blank' className='footer__social-link'><i class="uil uil-facebook-f"></i></a>
+          <a
+            href="https://www.linkedin.com/in/aom-kham-phaung-903291242"
+            target="blank"
+            className="footer__social-link"
+          >
+            <i class="uil uil-linkedin-alt"></i>
+          </a>
 
-                    <a href="https://github.com/aomkhamphaung" target='blank' className='footer__social-link'><i class="uil uil-github-alt"></i></a>
-                </div>
+          <a
+            href="https://github.com/aomkhamphaung"
+            target="blank"
+            className="footer__social-link"
+          >
+            <i class="uil uil-github-alt"></i>
+          </a>
+        </div>
 
-                <span className='footer__copy'>&#169; aomkhamphaung | All rights reserved</span>
-            </div>
-        </footer>
-    )
-}
+        <span className="footer__copy">
+          &#169; aomkhamphaung | All rights reserved
+        </span>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
--- src/components/home/Social.jsx
+++ src/components/home/Social.jsx
@@ -1,15 +1,33 @@
-import React from 'react'
+import React from "react";
 
 const Social = () => {
   return (
-    <div className='home__social'>
-        <a href="https://www.instagram.com/aom_kham_phaung" target='blank' className='home__social-icon'><i class="uil uil-instagram"></i></a>
+    <div className="home__social">
+      <a
+        href="mailto:aomkhamphaung.dev@gmail.com"
+        target="blank"
+        className="home__social-icon"
+      >
+        <i class="uil uil-envelope"></i>
+      </a>
 
-        <a href="https://www.facebook.com/aomkhamphaung" target='blank' className='home__social-icon'><i class="uil uil-facebook-f"></i></a>
+      <a
+        href="https://www.linkedin.com/in/aom-kham-phaung-903291242"
+        target="blank"
+        className="home__social-icon"
+      >
+        <i class="uil uil-linkedin-alt"></i>
+      </a>
 
-        <a href="https://github.com/aomkhamphaung" target='blank' className='home__social-icon'><i class="uil uil-github-alt"></i></a>
+      <a
+        href="https://github.com/aomkhamphaung"
+        target="blank"
+        className="home__social-icon"
+      >
+        <i class="uil uil-github-alt"></i>
+      </a>
     </div>
-  )
-}
+  );
+};
 
-export default Social
+export default Social;
