28cb1f9355ac04bc7c4b96a3d5b85d59e8ce519d
aomkhamphaung <akhamphaung@gmail.com>
2024-03-06 09:34:18 +0630

    remove: service component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React from "react";
-import './App.css';
+import "./App.css";
 import Header from "./components/header/Header";
 import Home from "./components/home/Home";
 import About from "./components/about/About";
 import Skills from "./components/skills/Skills";
-import Services from "./components/services/Services";
 import Qualification from "./components/qualification/Qualification";
 import Work from "./components/works/Work.jsx";
 import Contact from "./components/contact/Contact";
@@ -12,25 +11,23 @@ import Footer from "./components/footer/Footer";
 import ScrollUp from "./components/scrollup/ScrollUp";
 
 const App = () => {
-  return(
+  return (
     <>
       <Header />
-    
 
-    <main className="main">
-      <Home/>
-      <About/>
-      <Skills/>
-      <Services/>
-      <Qualification/>
-      <Work/>
-      <Contact/>
-    </main>
+      <main className="main">
+        <Home />
+        <About />
+        <Skills />
+        <Qualification />
+        <Work />
+        <Contact />
+      </main>
 
-    <Footer/>
-    <ScrollUp/>
+      <Footer />
+      <ScrollUp />
     </>
-  )
-}
+  );
+};
 
 export default App;
