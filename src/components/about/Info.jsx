55731535e9f443c87d5e512f246583634ecb8f1f
aomkhamphaung <akhamphaung@gmail.com>
2024-03-07 14:17:49 +0630

    added: icon animation

--- src/components/about/Info.jsx
+++ src/components/about/Info.jsx
@@ -3,19 +3,19 @@ import React from "react";
 const Info = () => {
   return (
     <div className="about__info grid">
-      <div className="about__box">
+      <div className="about__box bx-burst-hover">
         <i class="bx bx-award about__icon"></i>
         <h3 className="about__title">Experience</h3>
         <span className="about__subtitle">2 years+ in Web Development</span>
       </div>
 
-      <div className="about__box">
+      <div className="about__box bx-burst-hover">
         <i class="bx bx-briefcase-alt about__icon"></i>
         <h3 className="about__title">Working</h3>
         <span className="about__subtitle">1 year+ Working Experience</span>
       </div>
 
-      <div className="about__box">
+      <div className="about__box bx-burst-hover">
         <i class="bx bxs-graduation about__icon"></i>
         <h3 className="about__title">Certificates</h3>
         <span className="about__subtitle">20+ Professional Certificates</span>
--- src/components/skills/Frontend.jsx
+++ src/components/skills/Frontend.jsx
@@ -7,7 +7,7 @@ const Frontend = () => {
 
       <div className="skills__box">
         <div className="skills__group">
-          <div className="skills__data">
+          <div className="skills__data bx-tada-hover">
             <i class="bx bx-badge-check"></i>
             <div>
               <h3 className="skills__name">HTML</h3>
@@ -15,7 +15,7 @@ const Frontend = () => {
             </div>
           </div>
 
-          <div className="skills__data">
+          <div className="skills__data bx-tada-hover">
             <i class="bx bx-badge-check"></i>
             <div>
               <h3 className="skills__name">CSS</h3>
@@ -23,7 +23,7 @@ const Frontend = () => {
             </div>
           </div>
 
-          <div className="skills__data">
+          <div className="skills__data bx-tada-hover">
             <i class="bx bx-badge-check"></i>
             <div>
               <h3 className="skills__name">JavaScript</h3>
@@ -31,7 +31,7 @@ const Frontend = () => {
             </div>
           </div>
 
-          <div className="skills__data">
+          <div className="skills__data bx-tada-hover">
             <i class="bx bx-badge-check"></i>
             <div>
               <h3 className="skills__name">Bootstrap</h3>
@@ -41,7 +41,7 @@ const Frontend = () => {
         </div>
 
         <div className="skills__group">
-          <div className="skills__data">
+          <div className="skills__data bx-tada-hover">
             <i class="bx bx-badge-check"></i>
             <div>
               <h3 className="skills__name">TailwindCSS</h3>
@@ -49,7 +49,7 @@ const Frontend = () => {
             </div>
           </div>
 
-          <div className="skills__data">
+          <div className="skills__data bx-tada-hover">
             <i class="bx bx-badge-check"></i>
             <div>
               <h3 className="skills__name">ReactJS</h3>
@@ -57,7 +57,7 @@ const Frontend = () => {
             </div>
           </div>
 
-          <div className="skills__data">
+          <div className="skills__data bx-tada-hover">
             <i class="bx bx-badge-check"></i>
             <div>
               <h3 className="skills__name">VueJS</h3>
