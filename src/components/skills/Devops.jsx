23491b07d637fd9ca2cfd4f5da99852118385a7a
aomkhamphaung <akhamphaung@gmail.com>
2024-03-11 09:23:03 +0630

    improve: titles in skills section

--- src/components/skills/Devops.jsx
+++ src/components/skills/Devops.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Devops = () => {
   return (
     <div className="skills__content">
-      <h3 className="skills__title">Others</h3>
+      <h3 className="skills__title">Additional Skills</h3>
 
       <div className="skills__box">
         <div className="skills__group">
